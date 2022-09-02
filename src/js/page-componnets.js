16237334229ecc174f958d44cd5af987cd932190
Batcleta <thewazig.wf@gmail.com>
2022-09-02 11:15:54 -0300

    refactor: changing icons from navbar component

--- src/js/page-componnets.js
+++ src/js/page-componnets.js
@@ -9,40 +9,41 @@ class TopBar extends HTMLElement {
 }
 
 class Header extends HTMLElement {
-    connectedCallback() {
+    constructor() {
+        super()
         this.innerHTML = `
-        <div id="navbar" class="l-navbar">
+        <div id="navbar" class="slide-navbar">
         <div class="nav">
             <div>
                 <div class="nav__brand">
                     <!-- colocar icone de menu -->
-                    <i id="nav-toggle"></i>
+                    <i id="nav-toggle" class="icon ion-android-menu nav__toggle"></i>
                     <a href="#" class="nav__logo">
-                        <img src="" alt="logo integra store" srcset="">
+                        <img src="./src/img/integra-logo.svg" alt="logo integra store" srcset="">
                     </a>
                 </div>
 
                 <div class="nav__list">
                     <a href="" class="nav__link active nav__active">
-                        <i class="nav__icon"></i>
+                        <i class="nav__icon icon ion-clipboard " ></i>
                         <span class="nav__name">Dashboard</span>
                     </a>
                     <a href="" class="nav__link nav__active">
-                        <i class="nav__icon"></i>
+                    <i class="icon ion-android-people nav__icon"></i>
                         <span class="nav__name">Contatos</span>
                     </a>
                     <a href="" class="nav__link nav__active">
-                        <i class="nav__icon"></i>
+                    <i class="icon ion-ios-box nav__icon"></i>
                         <span class="nav__name">Estoque</span>
                     </a>
                     <a href="" class="nav__link nav__active">
-                        <i class="nav__icon"></i>
+                    <i class="icon ion-social-dropbox nav__icon"></i>
                         <span class="nav__name">Saídas</span>
                     </a>
                     <div class="nav__link nav_active nav_collapse">
-                        <i class="nav__icon"></i>
-                        <span class="nav__name"></span>
-                        <i class="collapse__link__arrow"></i>
+                    <i class="icon ion-person-add nav__icon"></i>
+                        <span class="nav__name">Cadastros</span>
+                        <i class="icon ion-android-arrow-dropdown collapse__link"></i>
                         <ul class="collapse__menu">
                             <li class="collapse__sublink">
                                 <a href="">Emitente</a>
@@ -68,7 +69,7 @@ class Header extends HTMLElement {
                 </div>
             </div>
             <a href="#" class="nav__link">
-                <svg class="nav__icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none">
+                <svg class="nav__icon icon ion-" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none">
                     <path d="M17 16L21 12M21 12L17 8M21 12L7 12M13 16V17C13 18.6569 11.6569 20 10 20H6C4.34315 20 3 18.6569 3 17V7C3 5.34315 4.34315 4 6 4H10C11.6569 4 13 5.34315 13 7V8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
                 <span class="nav__name">Sair</span>
