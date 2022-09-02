f45f010e60b23f8a1260f25d58a6ebf2e0fb19a8
Batcleta <thewazig.wf@gmail.com>
2022-09-02 11:16:44 -0300

    feat: creating dom element functions

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+
+const getDomIdElement = (element) => document.getElementById(element)
+const getDomClass = (element) => document.getElementsByClassName(element)
+const queryAll = (element) => document.querySelectorAll(element)
